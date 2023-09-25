07a8c16cd8859e6c49a06e66d142fed00f98f259
Pinninti Vasudeva Rao <143317532+Vasudev36818@users.noreply.github.com>
2023-09-25 07:46:31 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Combination - GFG/maximum-sum-combination.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int b[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            List<Integer> ans = obj.maxCombinations(n, k, a, b);
+            for (int e : ans) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+
+// User function Template for Java
+
+class Solution {
+    static List<Integer> maxCombinations(int N, int K, int A[], int B[]) {
+        // code here
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=N-1;i>=N-K;i--){
+            for(int j=N-1;j>=N-K;j--){
+                int sum=A[i]+B[j];
+                if(pq.size()<K){
+                   pq.add(A[i]+B[j]);
+                }else{
+                    if(pq.peek()<sum){
+                    pq.remove();
+                    pq.add(sum);
+                    }
+                    else{
+                       break;
+                    }
+                }
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.remove());
+        }
+        // System.out.println(pq);
+        Collections.sort(ans,Collections.reverseOrder());
+        return ans;
+    }
+}
