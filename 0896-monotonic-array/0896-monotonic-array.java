dbd5b199e11925507625ebf1daa43e4f4bc7e0d6
Pinninti Vasudeva Rao <143317532+Vasudev36818@users.noreply.github.com>
2023-09-29 12:34:00 +0530

    Time: 1 ms (99.82%), Space: 55.7 MB (52.33%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean flag=true;
-        boolean equal=true,flag1=false;
-
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]>nums[i]) flag=false;
-            if(nums[i-1]!=nums[i]) equal=false;
+        if (nums[0] < nums[nums.length-1]) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[i-1]) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] > nums[i-1]) {
+                    return false;
+                }
+            }
         }
-           for(int i=1;i<nums.length;i++){
-            if(nums[i-1]<nums[i]) flag1=true;
-        }
-        if(equal==true) return true;
-        if(flag==flag1) return true;
-        return false;
+        return true;
     }
 }
