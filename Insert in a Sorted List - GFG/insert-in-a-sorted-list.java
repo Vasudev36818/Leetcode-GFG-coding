1eb43916d8a7e192cfb373d2279fabb769a9c7e2
Pinninti Vasudeva Rao <143317532+Vasudev36818@users.noreply.github.com>
2023-10-08 12:04:37 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.java
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Create a new node for the key.
+        Node newNode = new Node(key);
+        
+       
+        
+        // Handle the case where the new node should be the new head.
+        if (key < head1.data || head1 == null) {
+            newNode.next = head1;
+            return newNode;
+        }
+        
+        // Traverse the list to find the correct position for insertion.
+        Node current = head1;
+        while (current.next != null && current.next.data < key) {
+            current = current.next;
+        }
+        
+        // Insert the new node between current and current.next.
+        newNode.next = current.next;
+        current.next = newNode;
+        
+        return head1;
+    }
+}
