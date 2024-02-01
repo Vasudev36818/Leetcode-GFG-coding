dbde8d82cb42f960f2db2f53f920d5a93718e3a7
Pinninti Vasudeva Rao <vasudevpinninti36818@gmail.com>
2024-02-01 21:11:11 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (26.85%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones=0,twos=0;
+
+        for(int i : nums){
+            ones=(i^ones)&(~twos);
+            twos=(i^twos)&(~ones);
+        }
+        return ones;
+        
+    }
+}
